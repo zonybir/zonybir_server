f6e56df6dfbd0ac4eb57599e3cae1608eddc46ae
zonybir <zonybir@icloud.com>
2017-08-23 10:54:22 +0800

    add default requset

--- routers/index.js
+++ routers/index.js
@@ -9,12 +9,11 @@ const router=(app,express)=>{
     app.use('*',(req,res,next)=>{
         next();
     });
-    app.get('/',(req,res)=>{
-        res.send('homePage');
-    });
-    
     app.use('/public',public);
     app.use('/user',User);
+    app.use('*',(req,res)=>{
+        res.status(500).json({code:500,message:'未授权',data:{}});
+    })
 }
 
 module.exports = router;
